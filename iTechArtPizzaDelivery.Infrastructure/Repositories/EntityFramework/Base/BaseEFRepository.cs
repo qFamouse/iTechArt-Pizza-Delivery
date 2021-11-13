0fb23f6d137798ff468a6f81b2db03957384b5df
Famouse <famouse_ru@mail.ru>
2021-11-13 13:10:28 +0300

    Add to base repository  mapper

--- iTechArtPizzaDelivery.Infrastructure/Repositories/EntityFramework/Base/BaseEFRepository.cs
+++ iTechArtPizzaDelivery.Infrastructure/Repositories/EntityFramework/Base/BaseEFRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using iTechArtPizzaDelivery.Infrastructure.Repositories.Context;
 
 namespace iTechArtPizzaDelivery.Infrastructure.Repositories.EntityFramework.Base
@@ -10,10 +11,12 @@ namespace iTechArtPizzaDelivery.Infrastructure.Repositories.EntityFramework.Base
     public class BaseEFRepository
     {
         private protected readonly PizzaDeliveryContext _dbContext;
+        private protected readonly IMapper _mapper;
 
-        public BaseEFRepository(PizzaDeliveryContext context)
+        public BaseEFRepository(PizzaDeliveryContext context, IMapper mapper)
         {
             _dbContext = context ?? throw new ArgumentNullException(nameof(context), "DB Context is null");
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper), "Mapper is null");
         }
     }
 }
