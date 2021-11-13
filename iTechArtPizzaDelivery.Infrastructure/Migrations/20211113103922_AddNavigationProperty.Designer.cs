69ba4149a49dace692004df1dedb2ffcc93f944c
Famouse <famouse_ru@mail.ru>
2021-11-13 16:26:23 +0300

    Migration for navigation property

--- iTechArtPizzaDelivery.Infrastructure/Migrations/20211113103922_AddNavigationProperty.Designer.cs
+++ iTechArtPizzaDelivery.Infrastructure/Migrations/20211113103922_AddNavigationProperty.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using iTechArtPizzaDelivery.Infrastructure.Repositories.Context;
 
 namespace iTechArtPizzaDelivery.Infrastructure.Migrations
 {
     [DbContext(typeof(PizzaDeliveryContext))]
-    partial class PizzaSizeContextModelSnapshot : ModelSnapshot
+    [Migration("20211113103922_AddNavigationProperty")]
+    partial class AddNavigationProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ iTechArtPizzaDelivery.Infrastructure/Migrations/20211113103922_AddNavigationProperty.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace iTechArtPizzaDelivery.Infrastructure.Migrations
+{
+    public partial class AddNavigationProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
