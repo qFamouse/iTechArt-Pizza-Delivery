a242632e339a068d672cadd0632e068289d729e1
qFamouse <famouse_ru@mail.ru>
2022-01-13 14:04:23 +0300

    Rename Profile folder to Mapping

--- iTechArtPizzaDelivery.WebUI/Mapping/OrderProfile.cs
+++ iTechArtPizzaDelivery.WebUI/Mapping/OrderProfile.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using iTechArtPizzaDelivery.Core.Entities;
 using iTechArtPizzaDelivery.WebUI.Views;
 
-namespace iTechArtPizzaDelivery.WebUI.Profiles
+namespace iTechArtPizzaDelivery.WebUI.Mapping
 {
     public class OrderProfile : Profile
     {
