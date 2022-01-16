99a9335def8e2a6901d31c59b84adb32897ba687
qFamouse <famouse_ru@mail.ru>
2022-01-16 21:29:23 +0300

    Adding Resource Configuration

---
+++ iTechArtPizzaDelivery.Core/Configurations/ResourceConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArtPizzaDelivery.Core.Configurations
+{
+    public class ResourceConfiguration
+    {
+        /// <summary>
+        /// Returns path to the application resources
+        /// </summary>
+        public string ApplicationDataPath => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) +
+                                             "\\" + ResourceName;
+        /// <summary>
+        /// Returns the name of the resource folder
+        /// </summary>
+        public string ResourceName { get; set; }
+
+        /// <summary>
+        /// Returns the name of the folder for pizza images
+        /// </summary>
+        public string PizzaImageName { get; set; }
+
+        /// <summary>
+        /// Returns limit of size for images
+        /// </summary>
+        public long ImageSizeLimit { get; set; }
+
+        /// <summary>
+        /// Return string array with allowed image extensions
+        /// </summary>
+        public string[] AllowedImageContentTypes { get; set; }
+
+    }
+}
