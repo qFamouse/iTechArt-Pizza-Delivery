4e889715de3aab87d4fd98bd4d2cc13a12faabf8
qFamouse <famouse_ru@mail.ru>
2022-01-18 18:17:06 +0300

    ToPageable. Add Extension method to DbSet for pagination

---
+++ iTechArtPizzaDelivery.Infrastructure/Extensions/DbSetExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace iTechArtPizzaDelivery.Infrastructure.Extensions
+{
+    static class DbSetExtensions
+    {
+        public static IQueryable<T> ToPageable<T>(this DbSet<T> dbSet, int pageNumber, int pageSize) where T : class
+        {
+            return dbSet.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
