2111884cd55060d82fdb784659f2504eb2e80956
qFamouse <famouse_ru@mail.ru>
2022-01-25 17:00:35 +0300

    Add mailer service

---
+++ iTechArtPizzaDelivery.Core/Interfaces/Services/IMailerService.cs
@@ -0,0 +1,14 @@
+﻿using iTechArtPizzaDelivery.Core.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArtPizzaDelivery.Core.Interfaces.Services
+{
+    public interface IMailerService
+    {
+        void SendMail(MailView mail);
+    }
+}
---
+++ iTechArtPizzaDelivery.Core/Services/MailerService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using iTechArtPizzaDelivery.Core.Interfaces.Services;
+using iTechArtPizzaDelivery.Core.Views;
+using RabbitMQ.Client;
+
+namespace iTechArtPizzaDelivery.Core.Services
+{
+    public class MailerService : IMailerService, IDisposable
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public MailerService(IRabbitMqService rabbitMqService)
+        {
+            _connection = rabbitMqService.CreateConnection() ?? throw new NullReferenceException(nameof(_connection));
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(
+                queue: "Mailer",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+        }
+
+        public void SendMail(MailView mail)
+        {
+            var body = Encoding.UTF8.GetBytes(mail.ToString());
+
+            _channel.BasicPublish(
+                exchange: "",
+                routingKey: "Mailer",
+                basicProperties: null,
+                body: body);
+        }
+
+        public void Dispose()
+        {
+            if (_channel.IsOpen)
+            {
+                _channel.Close();
+            }
+
+            if (_connection.IsOpen)
+            {
+                _connection.Close();
+            }
+        }
+    }
+}
