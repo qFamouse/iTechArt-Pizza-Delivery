487e90e794cc10d35749f94967b3f37fccab1a23
qFamouse <famouse_ru@mail.ru>
2022-01-25 17:00:12 +0300

    Add RabitMqService

---
+++ iTechArtPizzaDelivery.Core/Interfaces/Services/IRabbitMqService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace iTechArtPizzaDelivery.Core.Interfaces.Services
+{
+    public interface IRabbitMqService
+    {
+        public IConnection CreateConnection();
+    }
+}
